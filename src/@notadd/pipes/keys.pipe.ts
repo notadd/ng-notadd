06d4962f8f21e8eb196c43d8c0483b47063c70ab
天魂 <365125264@qq.com>
2018-09-30 19:08:44 +0800

    feat(@notadd): add pipes

---
+++ src/@notadd/pipes/keys.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'keys'
+})
+export class KeysPipe implements PipeTransform {
+
+    transform(value: any, args?: any): any {
+        const keys: Array<any> = [];
+
+        for (const key in value) {
+            if (value.hasOwnProperty(key)) {
+                keys.push({
+                    key,
+                    value: value[key]
+                });
+            }
+        }
+
+        return keys;
+    }
+
+}
---
+++ src/@notadd/pipes/pipes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { KeysPipe } from './keys.pipe';
+
+@NgModule({
+    imports: [
+    ],
+    declarations: [ KeysPipe ],
+    exports: [ KeysPipe ]
+})
+export class NotaddPipesModule {
+}
