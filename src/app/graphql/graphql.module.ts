0d4932ac5d7e240f285b8e383ccf8af9bd1df2ab
天魂 <365125264@qq.com>
2018-11-02 19:29:12 +0800

    feat(env): add apiUrl in environment

--- src/app/graphql/graphql.module.ts
+++ src/app/graphql/graphql.module.ts
@@ -3,7 +3,9 @@ import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const uri = 'http://localhost:3000/graphql'; // <-- add the URL of the GraphQL server here
+import { environment } from 'environments/environment';
+
+const uri = environment.apiUrl; // <-- add the URL of the GraphQL server here
 
 export function createApollo(httpLink: HttpLink) {
     return {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+    production: true,
+    apiUrl: 'https://ng-notadd.app.ibenchu.pw/graphql'
 };
