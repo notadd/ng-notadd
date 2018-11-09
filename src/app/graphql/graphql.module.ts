9c395b9cae55206bc0b4c18c76838f1284c47245
天魂 <365125264@qq.com>
2018-11-09 11:29:36 +0800

    feat(app/graphql): amend graphql module

--- src/app/graphql/graphql.module.ts
+++ src/app/graphql/graphql.module.ts
@@ -1,15 +1,68 @@
 import { NgModule } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
+import { onError } from 'apollo-link-error';
+import { NgForage } from 'ngforage';
 
 import { environment } from 'environments/environment';
 
 const uri = environment.apiUrl; // <-- add the URL of the GraphQL server here
 
-export function createApollo(httpLink: HttpLink) {
+export function createApollo(httpLink: HttpLink, ngForage: NgForage) {
+
+    // Http link
+    const http = httpLink.create({ uri });
+
+    // Cached storage for the user token
+    let token;
+
+    const withToken = setContext(async () => {
+        // if you have a cached value, return it immediately
+        if (token) { return token; }
+
+        token = await ngForage.getItem('TOKEN_DATA');
+    });
+
+    const resetToken = onError(({ networkError }) => {
+        if (networkError && networkError['statusCode'] === 401) {
+            // remove cached token on 401 from the server
+            token = '';
+        }
+    });
+
+    // Flow link
+    const authFlowLink = withToken.concat(resetToken);
+
+    // Middleware
+    const authMiddleware = new ApolloLink((operation, forward) => {
+        // add the authorization to the headers
+        operation.setContext({
+            headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
+        });
+
+        return forward(operation);
+    });
+
+    // Error link
+    const errorLink = onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors) {
+            graphQLErrors.map(({ message, locations, path }) =>
+                console.log(
+                    `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+                )
+            );
+        }
+        if (networkError) {
+            console.log(`[Network error]:`, networkError);
+        }
+    });
+
     return {
-        link: httpLink.create({uri}),
+        link: ApolloLink.from([authFlowLink, authMiddleware, errorLink, http]),
         cache: new InMemoryCache(),
     };
 }
@@ -23,9 +76,8 @@ export function createApollo(httpLink: HttpLink) {
         {
             provide: APOLLO_OPTIONS,
             useFactory: createApollo,
-            deps: [HttpLink],
+            deps: [ HttpLink, NgForage ],
         },
     ],
 })
-export class GraphQLModule {
-}
+export class GraphQLModule {}
