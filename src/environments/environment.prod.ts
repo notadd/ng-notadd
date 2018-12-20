47cff8bdc781b6c83a91e285fec6317f7ef416a9
小黄 <wish_cheng@163.com>
2018-12-20 19:45:44 +0800

    feat(environments): add caiyunApi url and key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,9 @@ export const environment = {
     apiUrl: 'https://ng-notadd.app.ibenchu.pw/graphql',
     reCaptcha: {
         siteKey: '6Lf4g4EUAAAAAEU7CoO7hdeF7VWj4BvHRThloq_K'
+    },
+    caiyunApi: {
+        url: 'https://caiyun-api.ibenchu.net/',
+        key: 'notadd'
     }
 };
