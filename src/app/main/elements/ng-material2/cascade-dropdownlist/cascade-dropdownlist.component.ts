dafe3503c1f09b51b5c241b4afbf530908f87aad
小黄 <wish_cheng@163.com>
2018-12-26 19:22:55 +0800

    feat(elements/ng-material2): add nm-cascade-dropdownlist demo component

---
+++ src/app/main/elements/ng-material2/cascade-dropdownlist/cascade-dropdownlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'cascade-dropdownlist',
+    templateUrl: './cascade-dropdownlist.component.html',
+    styleUrls: ['./cascade-dropdownlist.component.scss']
+})
+export class CascadeDropdownlistComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
