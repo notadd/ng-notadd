bc524ad21953ab8c2ff278f3e34252b6902274c2
小黄 <wish_cheng@163.com>
2018-12-29 20:55:26 +0800

    feat(elements/angular-cdk): add virtual list demo page

---
+++ src/app/main/elements/angular-cdk/virtual-list/virtual-list.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { VirtualListService } from './virtual-list.service';
+import { Observable } from 'rxjs';
+
+@Component({
+    selector: 'virtual-list',
+    templateUrl: './virtual-list.component.html',
+    styleUrls: ['./virtual-list.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class VirtualListComponent implements OnInit {
+
+    items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
+
+    content = [1];
+
+    fixedSizeData = this.virtualListService.FIXED_SIZE;
+
+    states$ = Observable.create(observer => {
+        observer.next(this.virtualListService.STATES);
+        observer.complete();
+    });
+
+    constructor(private virtualListService: VirtualListService) { }
+
+    ngOnInit() {
+    }
+
+    appendContent() {
+        this.content = this.content.concat(Array(5).fill(1));
+    }
+
+    sortBy(prop: 'name' | 'capital') {
+        const sortedVals = this.virtualListService.STATES.map(s => ({ ...s })).sort((a, b) => {
+            const aProp = a[prop], bProp = b[prop];
+            if (aProp < bProp) {
+                return -1;
+            } else if (aProp > bProp) {
+                return 1;
+            }
+            return 0;
+        });
+
+        this.states$ = Observable.create(observer => {
+            observer.next(sortedVals);
+            observer.complete();
+        });
+    }
+
+}
