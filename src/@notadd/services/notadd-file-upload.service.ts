01938384abba2791138f9415aed3084aefb34e95
天魂 <365125264@qq.com>
2019-01-09 19:15:23 +0800

    feat(@notadd/services): add notadd-file-upload.service

---
+++ src/@notadd/services/notadd-file-upload.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Observable } from 'rxjs';
+
+import { NotaddFileUploadDialogComponent } from '@notadd/components/file-upload/dialog/dialog.component';
+
+export interface UploadOptions {
+    accept?: string;
+    maxFileSize?: number;
+    maxFiles?: number;
+    fileCount?: number;
+    fileExtensions?: string;
+}
+
+@Injectable()
+export class NotaddFileUploadService {
+
+    constructor(
+        private dialog: MatDialog
+    ) {
+    }
+
+    upload(options?: UploadOptions): Observable<any> {
+        const dialogRef = this.dialog.open(NotaddFileUploadDialogComponent, {
+            width: '40%',
+            height: '50%',
+            data: options || {}
+        });
+
+        return dialogRef.afterClosed();
+    }
+}
