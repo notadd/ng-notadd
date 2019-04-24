e38df9015fe5bce5881a76a08ab9e72dafe18a67
天魂 <365125264@qq.com>
2019-04-24 15:43:39 +0800

    feat(layout): update dynamic footer year

--- src/app/layout/components/footer/footer.component.ts
+++ src/app/layout/components/footer/footer.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+    get copyrightYear() {
+        return new Date().getFullYear();
+    }
+
     constructor() {
     }
 
